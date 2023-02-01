ae713b71f3c269484e934a8ce412a8558042ae8f
JigyasuSharma <jigyasusharma2803@gmail.com>
2023-02-01 22:35:23 +0530

    move user side to main directory

---
+++ wishlist.php
@@ -0,0 +1,50 @@
+ <?php include("components/header.php"); ?>
+
+ <head>
+ 	<title>Wishlist</title>
+ </head>
+
+ <body>
+ 	<section class="breadcrumb-option" style="padding:15px 0 !important;" >
+ 		<div class="container">
+ 			<div class="row">
+ 				<div class="col-lg-12">
+ 					<div class="breadcrumb__text">
+ 						<h4>Shop</h4>
+ 						<div class="breadcrumb__links">
+ 							<a href="./index.php">Home</a>
+ 							<span>Shop</span>
+ 						</div>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</section>
+ 		<div class="container" style="padding-top: 20px; padding-bottom:20px">
+ 			<div class="row">
+ 				<div class="col-md-6">
+ 					<div class="card" style="width: 18rem;">
+ 						<img class="card-img-top" src="..." alt="Card image cap">
+ 						<div class="card-body">
+ 							<h5 class="card-title">Card title</h5>
+ 							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+ 						</div>
+ 						<ul class="list-group list-group-flush">
+ 							<li class="list-group-item">Cras justo odio</li>
+ 							<li class="list-group-item">Dapibus ac facilisis in</li>
+ 							<li class="list-group-item">Vestibulum at eros</li>
+ 						</ul>
+ 						<div class="card-body">
+ 							<a href="#" class="card-link">Card link</a>
+ 							<a href="#" class="card-link">Another link</a>
+ 						</div>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+ 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ </body>
+
+ </html>
+ <?php include("components/footer.php"); ?>
