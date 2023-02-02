03b294e0968bc8802ac6d8e4bb425fea038d7c57
Avi <67220821+avi-lime@users.noreply.github.com>
2023-02-02 23:44:15 +0530

    bug fix

--- admin/template/header.php
+++ admin/template/header.php
@@ -5,8 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include("../global/html/require.html") ?>
-    <title>MementoAdmin</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/panel.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <title>Memento Admin</title>
     <link rel="icon" type="image/x-icon" href="../img/mxm-white.png" sizes="any">
 </head>
 
@@ -64,6 +77,12 @@
                         <a href="subcat.php">
                             <i class="fas fa-folder"></i>
                             <span class="links_name">Sub-Categories</span>
+                        </a>
+                    </li>
+                    <li class="item" data-bs-toggle="tooltip" data-bs-title="Products" data-bs-placement="right">
+                        <a href="product.php">
+                            <i class="fa-solid fa-clipboard-list"></i>
+                            <span class="links_name">Products</span>
                         </a>
                     </li>
                     <li class="item" data-bs-toggle="tooltip" data-bs-title="Users" data-bs-placement="right">
