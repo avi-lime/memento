5368d580a3ca0b28f644d5777faa6f2e3b66c58c
Avi <67220821+avi-lime@users.noreply.github.com>
2023-02-03 18:59:58 +0530

    Update memento.sql

--- memento.sql
+++ memento.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 02, 2023 at 03:48 PM
+-- Generation Time: Feb 03, 2023 at 01:29 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -35,10 +35,8 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `username` varchar(50) NOT NULL,
   `superadmin` int(1) DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `id` (`id`),
-  KEY `id_2` (`id`),
-  KEY `id_3` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -51,9 +49,7 @@ INSERT INTO `admin` (`id`, `email`, `password`, `username`, `superadmin`) VALUES
 (4, 'allahhuakbar911@boomboom.com', '$2y$10$8MWn2jmfEz5BkAGgR6NSqel7gmEtLIZM.EurMbCp9VxY4mNtgMiN.', 'allah hu akbar', 0),
 (5, 'vomajabij@mailinator.com', '$2y$10$9TQHj7jJ3hGZLiKHugcHHO34wRiAWCnN5uF1Qs51eqteMQE2T26kC', 'Ria Nash', 0),
 (6, 'gotihovusy@mailinator.com', '$2y$10$X.8WTy0GpCNHe5W0ZYv.cerZHVUrXb10H5JXEu6J8Kwx4eq87rE1S', 'Geoffrey Hernandez', 0),
-(7, 'bemah@mailinator.com', '$2y$10$OmtantmKfK6cGjp9eMpt3OsJdDiCi3a4gnYVtEdx.7I5xVRhwR/TG', 'Maxwell Gonzalez', 0),
-(8, 'xitezum@mailinator.com', '$2y$10$W/sS6/Non3lucszAXbUwheJlJNoYZQIncHpwTe/5m/P8sQibiXiM6', 'Iliana Greer', 0),
-(9, 'mysudemoh@mailinator.com', '$2y$10$D13vVkv3m6hnTGmvVyOYD.CijycmJJIJaJOLg.vaujnRI7ugc6Fg2', 'Idola Ryan', 0);
+(7, 'bemah@mailinator.com', '$2y$10$OmtantmKfK6cGjp9eMpt3OsJdDiCi3a4gnYVtEdx.7I5xVRhwR/TG', 'Maxwell Gonzalez', 0);
 
 -- --------------------------------------------------------
 
@@ -66,17 +62,18 @@ CREATE TABLE IF NOT EXISTS `category` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `name`, `image`) VALUES
-(11, 'Shirt', 'Screenshot (4).png'),
 (3, 'Tees', 'Screenshot (6).png'),
 (10, 'rahul', 'Screenshot (4).png'),
+(11, 'Shirt', 'Screenshot (4).png'),
 (12, 'Casual wear', 'autri-taheri-QAwtEAY6V8I-unsplash.jpg');
 
 -- --------------------------------------------------------
@@ -87,27 +84,44 @@ INSERT INTO `category` (`id`, `name`, `image`) VALUES
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
-  `productid` int(10) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(255) NOT NULL,
-  `product_price` int(20) NOT NULL,
-  `product_quantity` int(20) NOT NULL,
-  `product_desc` text NOT NULL,
-  `product_dis` int(3) NOT NULL,
-  `catid` int(10) NOT NULL,
-  `subcatid` int(10) NOT NULL,
-  `product_companyid` int(20) NOT NULL,
-  `product_img` varchar(255) NOT NULL,
-  `product_series` varchar(255) NOT NULL,
-  PRIMARY KEY (`productid`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` int(20) NOT NULL,
+  `quantity` int(20) NOT NULL,
+  `description` text NOT NULL,
+  `discount` int(3) NOT NULL,
+  `cat_id` int(10) NOT NULL,
+  `subcat_id` int(10) NOT NULL,
+  `company_id` int(20) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `series` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_product_cat` (`cat_id`),
+  KEY `FK_product_sub` (`subcat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productid`, `product_name`, `product_price`, `product_quantity`, `product_desc`, `product_dis`, `catid`, `subcatid`, `product_companyid`, `product_img`, `product_series`) VALUES
+INSERT INTO `product` (`id`, `name`, `price`, `quantity`, `description`, `discount`, `cat_id`, `subcat_id`, `company_id`, `img`, `series`) VALUES
 (6, 'Captain America: Colourblock', 2000, 800, 'Official Licensed Captain America Oversized Full Sleeve T-shirt.\r\n\r\nAre you ready to take your Streetwear game to the next level? Are you ready to have all the attention on you?\r\n\r\nIf yes, then you definitely need this ultra-stylish and extraordinarily comfortable Oversized Tee! And what better one than the one with Captain America\'s Shield on it which automatically makes it 10x cooler!\r\n\r\nBelieve us when we say that this is truly one of the most good-looking pieces of clothing you will ever own simply because of how innovative and different it is!\r\n\r\nWith our trademark quirky designs and unbelievably great quality, we guarantee that you will always look your best wherever you go!\r\n\r\nBe the trendsetter in your group and grab this tee today!\r\n\r\nShop for the best T-shirts online, exclusively at The Souled Store!', 12, 3, 2, 1, 'Screenshot (5).png', 'Captain America'),
 (7, 'Batman: Go Hawai', 2500, 500, 'Official Licensed Batman Summer Shirt\r\n\r\nWhen Batman requested us to make him a premium shirt, we couldn\'t deny him! Presenting to you this super-stylish and comfortable Batman shirt.\r\n\r\nBuy Batman Shirts online, available in India only at The Souled Store.', 0, 11, 10, 1, 'Screenshot (6).png', 'Batman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+DROP TABLE IF EXISTS `product_images`;
+CREATE TABLE IF NOT EXISTS `product_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_product_image` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -121,8 +135,10 @@ CREATE TABLE IF NOT EXISTS `subcat` (
   `name` varchar(50) NOT NULL,
   `cat_id` int(10) NOT NULL,
   `image` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `FK_sub_cat` (`cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subcat`
@@ -153,6 +169,29 @@ CREATE TABLE IF NOT EXISTS `user` (
   `img` blob NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `FK_product_cat` FOREIGN KEY (`cat_id`) REFERENCES `category` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_product_sub` FOREIGN KEY (`subcat_id`) REFERENCES `subcat` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD CONSTRAINT `FK_product_image` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `subcat`
+--
+ALTER TABLE `subcat`
+  ADD CONSTRAINT `FK_sub_cat` FOREIGN KEY (`cat_id`) REFERENCES `category` (`id`) ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
