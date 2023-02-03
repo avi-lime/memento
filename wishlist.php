0de1856c201c26e761df3b7d069c9cdf06e7ae0f
JigyasuSharma <jigyasusharma2803@gmail.com>
2023-02-03 23:28:23 +0530

    just design

--- wishlist.php
+++ wishlist.php
@@ -20,31 +20,31 @@
  			</div>
  		</div>
  	</section>
+	<?php
+	$sql='SELECT * FROM wishlist WHERE user_id=';
+	?>
  		<div class="container" style="padding-top: 20px; padding-bottom:20px">
  			<div class="row">
  				<div class="col-md-6">
  					<div class="card" style="width: 18rem;">
- 						<img class="card-img-top" src="..." alt="Card image cap">
- 						<div class="card-body">
- 							<h5 class="card-title">Card title</h5>
- 							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
- 						</div>
+					 <img class="card-img-top" src="img/product/product-1.jpg" alt="Card image cap">
+					 <div class="card-img-overlay">
+					 <a href="#" class="card-link"><i class="fa fa-trash" aria-hidden="true"></i></a>
+					 </div>
  						<ul class="list-group list-group-flush">
- 							<li class="list-group-item">Cras justo odio</li>
- 							<li class="list-group-item">Dapibus ac facilisis in</li>
- 							<li class="list-group-item">Vestibulum at eros</li>
+ 							<li class="list-group-item"></li>
  						</ul>
  						<div class="card-body">
- 							<a href="#" class="card-link">Card link</a>
- 							<a href="#" class="card-link">Another link</a>
+ 							<a href="#" class="card-link">Add To Bag</a>
  						</div>
  					</div>
  				</div>
  			</div>
  		</div>
- 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
- 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<?php
+		?>
  </body>
-
+ <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  </html>
  <?php include("components/footer.php"); ?>
