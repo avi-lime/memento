08cb60d897bfef567af19c6b3659f57b2e8eb0c5
JigyasuSharma <jigyasusharma2803@gmail.com>
2023-02-24 20:30:37 +0530

    eqqw

--- shop-details.php
+++ shop-details.php
@@ -104,13 +104,13 @@ if (($id = $_REQUEST['product_id']) && (isset($_REQUEST['product_id']))) {
                             <span> - 10 Reviews</span>
                         </div>
                         <!-- rating -->
-                        <h3>-<?php $discountrate = $detail['discount'];
-                                echo $discountrate
-                                ?>% &nbsp &nbsp &nbsp ₹<?php $originalprice = $detail['price'];
-                                                        $discountprice = $originalprice * ($discountrate / 100);
-                                                        $price = $originalprice - $discountprice;
-                                                        echo $price;
-                                                        ?><span>₹<?php echo $detail['price']; ?></span></h3>
+                        <h3><span>(-<?php $discountrate = $detail['discount'];
+                                    echo $discountrate
+                                    ?>% OFF)</span>₹<?php $originalprice = $detail['price'];
+                                                $discountprice = $originalprice * ($discountrate / 100);
+                                                $price = $originalprice - $discountprice;
+                                                echo $price;
+                                                ?><span>₹<?php echo $detail['price']; ?></span></h3>
                         <h5 style="font-weight: lighter; padding-bottom:10px">Please Select a Size</h5>
                         <div class="product__details__option">
                             <div class="product__details__option__size">
@@ -243,21 +243,26 @@ if (($id = $_REQUEST['product_id']) && (isset($_REQUEST['product_id']))) {
             </div>
         </div>
         <div class="row">
-            <?php 
-            
+            <?php
+            $relateditemsql = 'SELECT * FROM product WHERE cat_id=' . $detail['cat_id'] . '';
+            $result = mysqli_query($conn, $relateditemsql);
+            while ($relateditem = mysqli_fetch_assoc($result)) {
+                if($relateditem['id']>=$id){
             ?>
             <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
                 <div class="product__item">
-                    <div class="product__item__pic set-bg" data-setbg="img/product/product-1.jpg">
-                        <span class="label">New</span>
+                    <div class="product__item__pic set-bg" data-setbg="global/assets/images/<?php 
+                    $sql = 'SELECT image FROM product_images WHERE product_id = "' . $detail['id'] . '" LIMIT 1';
+                    $image = mysqli_fetch_assoc(mysqli_query($conn, $sql));
+                    echo $image['image'];
+                    ?>">
+                        <!-- <span class="label">New</span> -->
                         <ul class="product__hover">
-                            <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                            <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
-                            <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            <li><a href="" class="wishlist" id="<?php echo $detail['id'] ?>"><img src="img/icon/heart.png" alt=""></a></li>
                         </ul>
                     </div>
                     <div class="product__item__text">
-                        <h6>Piqué Biker Jacket</h6>
+                        <h6><?php echo $detail['name'] ?></h6>
                         <a href="#" class="add-cart">+ Add To Cart</a>
                         <div class="rating">
                             <i class="fa fa-star-o"></i>
@@ -267,125 +272,28 @@ if (($id = $_REQUEST['product_id']) && (isset($_REQUEST['product_id']))) {
                             <i class="fa fa-star-o"></i>
                         </div>
                         <h5>$67.24</h5>
-                        <div class="product__color__select">
-                            <label for="pc-1">
-                                <input type="radio" id="pc-1">
-                            </label>
-                            <label class="active black" for="pc-2">
-                                <input type="radio" id="pc-2">
-                            </label>
-                            <label class="grey" for="pc-3">
-                                <input type="radio" id="pc-3">
-                            </label>
-                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
-                <div class="product__item">
-                    <div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
-                        <ul class="product__hover">
-                            <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                            <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
-                            <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
-                        </ul>
-                    </div>
-                    <div class="product__item__text">
-                        <h6>Piqué Biker Jacket</h6>
-                        <a href="#" class="add-cart">+ Add To Cart</a>
-                        <div class="rating">
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                        </div>
-                        <h5>$67.24</h5>
-                        <div class="product__color__select">
-                            <label for="pc-4">
-                                <input type="radio" id="pc-4">
-                            </label>
-                            <label class="active black" for="pc-5">
-                                <input type="radio" id="pc-5">
-                            </label>
-                            <label class="grey" for="pc-6">
-                                <input type="radio" id="pc-6">
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
-                <div class="product__item sale">
-                    <div class="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
-                        <span class="label">Sale</span>
-                        <ul class="product__hover">
-                            <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                            <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
-                            <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
-                        </ul>
-                    </div>
-                    <div class="product__item__text">
-                        <h6>Multi-pocket Chest Bag</h6>
-                        <a href="#" class="add-cart">+ Add To Cart</a>
-                        <div class="rating">
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star-o"></i>
-                        </div>
-                        <h5>$43.48</h5>
-                        <div class="product__color__select">
-                            <label for="pc-7">
-                                <input type="radio" id="pc-7">
-                            </label>
-                            <label class="active black" for="pc-8">
-                                <input type="radio" id="pc-8">
-                            </label>
-                            <label class="grey" for="pc-9">
-                                <input type="radio" id="pc-9">
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
-                <div class="product__item">
-                    <div class="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
-                        <ul class="product__hover">
-                            <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                            <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
-                            <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
-                        </ul>
-                    </div>
-                    <div class="product__item__text">
-                        <h6>Diagonal Textured Cap</h6>
-                        <a href="#" class="add-cart">+ Add To Cart</a>
-                        <div class="rating">
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                            <i class="fa fa-star-o"></i>
-                        </div>
-                        <h5>$60.9</h5>
-                        <div class="product__color__select">
-                            <label for="pc-10">
-                                <input type="radio" id="pc-10">
-                            </label>
-                            <label class="active black" for="pc-11">
-                                <input type="radio" id="pc-11">
-                            </label>
-                            <label class="grey" for="pc-12">
-                                <input type="radio" id="pc-12">
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php
+            }
+        }
+            ?>
         </div>
     </div>
 </section>
+<!-- script -->
+<script>
+    $(".wishlist").click(function(e) {
+  e.preventDefault();
+  let id = $(this).attr("id");
+  $.ajax({
+    url: "api/wishlist.php",
+    data: {
+      id: id
+    }
+  })
+})
+</script>
 <!-- Related Section End -->
 <?php include("components/footer.php"); ?>
