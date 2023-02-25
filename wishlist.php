008fab9ca5577fe0b262095b4d0677bbb8bf9995
JigyasuSharma <jigyasusharma2803@gmail.com>
2023-02-25 16:25:02 +0530

    show details

--- wishlist.php
+++ wishlist.php
@@ -36,13 +36,26 @@
 				?>
  					<div class="col-md-3">
  						<div class="card" style="width: 18rem;">
- 							<img class="card-img-top" src="img/product/<?php echo $wishlistdetails['img'] ?>" style="height: 250px;" alt="Card image cap">
+						<a href="shop-details.php?product_id=<?php echo $wishlistdetails['id'] ?>">
+ 							<img class="card-img-top" src="global/assets/images/
+							<?php
+							$sql = 'SELECT image FROM product_images WHERE product_id = "' . $wishlist['product_id'] . '" LIMIT 1';
+							$image = mysqli_fetch_assoc(mysqli_query($conn, $sql));
+							echo $image['image']; ?>
+							" style="height: 350px;" alt="<?php echo $wishlistdetails['name'] ?>">
+							</a>
  							<div class="card-img-overlay">
  								<a href="#" class="card-link"><i class="fa fa-trash" aria-hidden="true"></i></a>
  							</div>
  							<ul class="list-group list-group-flush">
- 								<li class="list-group-item"><a href="shop-details.php?id=<?php echo $wishlistdetails['id'] ?>"><?php echo $wishlistdetails['name'] ?></a></li>
- 								<li class="list-group-item"><?php echo $wishlistdetails['price'] ?></li>
+ 								<li class="list-group-item" style="border:none"><?php echo $wishlistdetails['name'] ?></li>
+ 								<li class="list-group-item">₹
+ 									<?php $originalprice = $wishlistdetails['price'];
+										$discountrate = $wishlistdetails['discount'];
+										$discountprice = $originalprice * ($discountrate / 100);
+										$price = $originalprice - $discountprice;
+										echo $price;
+										?><span style="color: #b7b7b7;font-size: 15px;font-weight: 400;margin-left: 10px;text-decoration: line-through">₹<?php echo $wishlistdetails['price'] ?></span></li>
  							</ul>
  							<div class="card-body">
  								<a href="index.php" class="card-link">Add To Bag</a>
