969576ff1a64f41f760bdc4110c2171a370fe78c
Avi <67220821+avi-lime@users.noreply.github.com>
2023-02-27 16:06:19 +0530

    bootstrap v5.3 cdn

--- components/footer.php
+++ components/footer.php
@@ -87,7 +87,9 @@
 <!-- Search End -->
 
 <!-- Js Plugins -->
-<script src="js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
 <script src="js/jquery.nice-select.min.js"></script>
 <script src="js/jquery.nicescroll.min.js"></script>
 <script src="js/jquery.magnific-popup.min.js"></script>
