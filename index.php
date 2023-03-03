9820373a22704998a0647bec7f78740522caaee6
Avi <67220821+avi-lime@users.noreply.github.com>
2023-03-03 22:39:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -153,13 +153,6 @@ $result = mysqli_query($conn, $sql);
                                 <?php echo $relateditem['name'] ?>
                             </h6>
                             <a href="#" class="add-cart">+ Add To Cart</a>
-                            <div class="rating">
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
                             <h5>₹
                                 <?php $originalprice = $relateditem['price'];
                                 $discountrate = $relateditem['discount'];
@@ -177,58 +170,6 @@ $result = mysqli_query($conn, $sql);
             }
             ?>
         </div>
-
-        <?php
-        while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-
-            <div class="row product__filter">
-                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg">
-                            <img src="../global/assets/images/<?php echo $row["img"] ?>" alt="" class="card-img-top"
-                                style="object-fit: cover" height="300px">
-                            <span class="label">New</span>
-                            <ul class="product__hover">
-                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
-                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
-                            </ul>
-                        </div>
-                        <div class="product__item__text">
-                            <h5 class="card-title">
-                                <?php echo $row["name"] ?>
-                            </h5>
-                            <a href="#" class="add-cart">+ Add To Cart</a>
-                            <div class="rating">
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
-                            <b>Price:</b> ₹
-                            <?php echo $row["price"] ?>
-                            <div class="product__color__select">
-                                <label for="pc-1">
-                                    <input type="radio" id="pc-1">
-                                </label>
-                                <label class="active black" for="pc-2">
-                                    <input type="radio" id="pc-2">
-                                </label>
-                                <label class="grey" for="pc-3">
-                                    <input type="radio" id="pc-3">
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <?php
-        }
-        ?>
-
     </div>
 </section>
 <!-- Instagram Section End -->
