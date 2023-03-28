7059e44bc98b653bcdd2ac761c6c8a44d322577a
Avi <67220821+avi-lime@users.noreply.github.com>
2023-03-28 21:40:46 +0530

    Update shop.php

--- shop.php
+++ shop.php
@@ -44,78 +44,179 @@
                         <a data-bs-toggle="collapse" data-bs-target="#collapseThree">Filter Price</a>
                     </div>
                     <div id="collapseThree" class="collapse show">
-                        <div class="card-body">
-                            <div class="shop__sidebar__price">
-                                <ul>
-                                    <li><a href="#">$0.00 - $50.00</a></li>
-                                    <li><a href="#">$50.00 - $100.00</a></li>
-                                    <li><a href="#">$100.00 - $150.00</a></li>
-                                    <li><a href="#">$150.00 - $200.00</a></li>
-                                    <li><a href="#">$200.00 - $250.00</a></li>
-                                    <li><a href="#">250.00+</a></li>
-                                </ul>
+                        <div class="price-input">
+                            <div class="field">
+                                <span>Min</span>
+                                <input type="number" class="input-min" value="0">
                             </div>
+                            <div class="separator">-</div>
+                            <div class="field">
+                                <span>Max</span>
+                                <input type="number" class="input-max" value="2000">
+                            </div>
+                        </div>
+                        <div class="slider">
+                            <div class="progress"></div>
+                        </div>
+                        <div class="range-input">
+                            <input type="range" class="range-min" min="0" max="2000" value="0" step="10">
+                            <input type="range" class="range-max" min="0" max="2000" value="1000" step="10">
                         </div>
                     </div>
                 </div>
-                <div class="card bg-black">
-                    <div class="card-heading">
-                        <a data-bs-toggle="collapse" data-bs-target="#collapseFour">Size</a>
-                    </div>
-                    <div id="collapseFour" class="collapse show">
-                        <div class="card-body">
-                            <div class="shop__sidebar__size">
-                                <label for="sm">s
-                                    <input type="checkbox" name="size" id="sm" value="sm">
-                                </label>
-                                <label for="md">m
-                                    <input type="checkbox" name="size" id="md" value="md">
-                                </label>
-                                <label for="l">l
-                                    <input type="checkbox" name="size" id="l" value="l">
-                                </label>
-                            </div>
+
+                <!-- CSS for slider  -->
+
+                <style>
+                    .price-input {
+                        width: 100%;
+                        display: flex;
+                        margin: 30px 0 35px;
+                    }
+
+                    .price-input .field {
+                        display: flex;
+                        width: 100%;
+                        height: 45px;
+                        align-items: center;
+                    }
+
+                    .field input {
+                        width: 100%;
+                        height: 100%;
+                        outline: none;
+                        font-size: 19px;
+                        margin-left: 12px;
+                        border-radius: 5px;
+                        text-align: center;
+                        border: 1px solid #999;
+                        -moz-appearance: textfield;
+                    }
+
+                    input[type="number"]::-webkit-outer-spin-button,
+                    input[type="number"]::-webkit-inner-spin-button {
+                        -webkit-appearance: none;
+                    }
+
+                    .price-input .separator {
+                        width: 130px;
+                        display: flex;
+                        font-size: 19px;
+                        align-items: center;
+                        justify-content: center;
+                    }
+
+                    .slider {
+                        height: 5px;
+                        position: relative;
+                        background: #ddd;
+                        border-radius: 5px;
+                    }
+
+                    .slider .progress {
+                        height: 100%;
+                        left: 25%;
+                        right: 25%;
+                        position: absolute;
+                        border-radius: 5px;
+                        background: #fff;
+                    }
+
+                    .range-input {
+                        position: relative;
+                    }
+
+                    .range-input input {
+                        position: absolute;
+                        width: 100%;
+                        height: 5px;
+                        top: -5px;
+                        background: none;
+                        pointer-events: none;
+                        -webkit-appearance: none;
+                        -moz-appearance: none;
+                    }
+
+                    input[type="range"]::-webkit-slider-thumb {
+                        height: 17px;
+                        width: 17px;
+                        border-radius: 50%;
+                        background: #fff;
+                        pointer-events: auto;
+                        -webkit-appearance: none;
+                        box-shadow: 0 0 6px rgba(0, 0, 0, 0.05);
+                    }
+
+                    input[type="range"]::-moz-range-thumb {
+                        height: 17px;
+                        width: 17px;
+                        border: none;
+                        border-radius: 50%;
+                        background: #fff;
+                        pointer-events: auto;
+                        -moz-appearance: none;
+                        box-shadow: 0 0 6px rgba(0, 0, 0, 0.05);
+                    }
+                </style>
+            </div>
+            <div class="card bg-black">
+                <div class="card-heading">
+                    <a data-bs-toggle="collapse" data-bs-target="#collapseFour">Size</a>
+                </div>
+                <div id="collapseFour" class="collapse show">
+                    <div class="card-body">
+                        <div class="shop__sidebar__size">
+                            <label for="sm">s
+                                <input type="checkbox" name="size" id="sm" value="sm">
+                            </label>
+                            <label for="md">m
+                                <input type="checkbox" name="size" id="md" value="md">
+                            </label>
+                            <label for="l">l
+                                <input type="checkbox" name="size" id="l" value="l">
+                            </label>
                         </div>
                     </div>
                 </div>
-                <div class="card bg-black">
-                    <div class="card-heading">
-                        <a data-bs-toggle="collapse" data-bs-target="#collapseSix">Sub-Category</a>
-                    </div>
-                    <div id="collapseSix" class="collapse show" aria-expanded="true">
-                        <div class="card-body">
-                            <div class="shop__sidebar__tags">
-                                <?php
-                                if ($catid = isset($_REQUEST['cat_id'])) {
-                                    $subquery = 'SELECT * FROM subcat WHERE cat_id=' . $catid . '';
-                                    if ($result = mysqli_query($conn, $subquery)) {
-                                        while ($row = mysqli_fetch_assoc($result)) {
-                                            ?>
-                                            <a href="shop?sub_id=<?= $row['id'] ?>"><?php
-                                              echo $row['name'] ?></a>
-                                            <?php
-                                        }
-                                    }
-                                } else {
-                                    $query = 'SELECT * FROM subcat';
-                                    if ($result = mysqli_query($conn, $query)) {
-                                        while ($row = mysqli_fetch_assoc($result)) {
-                                            ?>
-                                            <a href="shop?sub_id=<?= $row['id'] ?>"><?php
-                                              echo $row['name'] ?></a>
-                                            <?php
-                                        }
+            </div>
+            <div class="card bg-black">
+                <div class="card-heading">
+                    <a data-bs-toggle="collapse" data-bs-target="#collapseSix">Sub-Category</a>
+                </div>
+                <div id="collapseSix" class="collapse show" aria-expanded="true">
+                    <div class="card-body">
+                        <div class="shop__sidebar__tags">
+                            <?php
+                            if ($catid = isset($_REQUEST['cat_id'])) {
+                                $subquery = 'SELECT * FROM subcat WHERE cat_id=' . $catid . '';
+                                if ($result = mysqli_query($conn, $subquery)) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        ?>
+                                        <a href="shop?sub_id=<?= $row['id'] ?>"><?php
+                                          echo $row['name'] ?></a>
+                                        <?php
                                     }
                                 }
-                                ?>
-                            </div>
+                            } else {
+                                $query = 'SELECT * FROM subcat';
+                                if ($result = mysqli_query($conn, $query)) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        ?>
+                                        <a href="shop?sub_id=<?= $row['id'] ?>"><?php
+                                          echo $row['name'] ?></a>
+                                        <?php
+                                    }
+                                }
+                            }
+                            ?>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
+
+</div>
 </div>
 
 
@@ -322,6 +423,45 @@
             e.preventDefault();
             location.href = "login";
         })
+
+        const rangeInput = document.querySelectorAll(".range-input input"),
+            priceInput = document.querySelectorAll(".price-input input"),
+            range = document.querySelector(".slider .progress");
+        let priceGap = 100;
+        priceInput.forEach(input => {
+            input.addEventListener("input", e => {
+                let minPrice = parseInt(priceInput[0].value),
+                    maxPrice = parseInt(priceInput[1].value);
+
+                if ((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max) {
+                    if (e.target.className === "input-min") {
+                        rangeInput[0].value = minPrice;
+                        range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+                    } else {
+                        rangeInput[1].value = maxPrice;
+                        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+                    }
+                }
+            });
+        });
+        rangeInput.forEach(input => {
+            input.addEventListener("input", e => {
+                let minVal = parseInt(rangeInput[0].value),
+                    maxVal = parseInt(rangeInput[1].value);
+                if ((maxVal - minVal) < priceGap) {
+                    if (e.target.className === "range-min") {
+                        rangeInput[0].value = maxVal - priceGap
+                    } else {
+                        rangeInput[1].value = minVal + priceGap;
+                    }
+                } else {
+                    priceInput[0].value = minVal;
+                    priceInput[1].value = maxVal;
+                    range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+                    range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                }
+            });
+        });
     })
 </script>
 <?php include("components/footer.php"); ?>
