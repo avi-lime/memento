052e0d48f8534838f187573c81a39a81f4e797bc
Avi <67220821+avi-lime@users.noreply.github.com>
2023-03-28 21:41:02 +0530

    kuch to copy paste kiya tha mkc

---
+++ api/fetch.php
@@ -0,0 +1,12 @@
+<?php
+include("../global/api/conn.php");
+if (isset($_REQUEST['query'])) {
+    $query = $_REQUEST["query"];
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+    $array = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($array, json_encode($row));
+    }
+    echo json_encode($array);
+}
+mysqli_close($conn);
