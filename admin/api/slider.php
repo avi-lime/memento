fd9a5283e4467f75f2fef68d2e8e6df2866fe7fb
JigyasuSharma <jigyasusharma2803@gmail.com>
2023-03-29 22:41:40 +0530

    Update slider.php

--- admin/api/slider.php
+++ admin/api/slider.php
@@ -19,14 +19,14 @@ if (!isset($_POST["name"])) {
             // fetch old image
             $result = mysqli_query($conn, "SELECT image FROM slider WHERE id=" . $_POST['id']);
             $row = mysqli_fetch_assoc($result);
-            $oldPicture = "../../global/assets/images/" . $row["image"];
+            $oldPicture = "../../global/assets/slider/" . $row["image"];
             unlink($oldPicture);
         }
 
         // upload new file
         $temp = explode(".", $_FILES["imgfile"]["name"]);
         $image = round(microtime(true)) . '.' . end($temp);
-        move_uploaded_file($_FILES["imgfile"]["tmp_name"], "../../global/assets/images/" . $image);
+        move_uploaded_file($_FILES["imgfile"]["tmp_name"], "../../global/assets/slider/" . $image);
 
     } else {
         $image = "default.png";
